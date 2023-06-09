e8cfcbb9099b7a566966a97e315fba328c8f48c4
Je Ar Becerro <jearbecerro@gmail.com>
2023-06-09 09:39:00 +0800

    add

--- app.py
+++ app.py
@@ -26,6 +26,18 @@ def hello_world():
 
 @app.route("/detect", methods=['POST'])
 def detect():
+    video = request.files['video']
+    video.save(os.path.join(uploads_dir, secure_filename(video.filename)))
+    print(video)
+    #subprocess.run("ls", shell=True)
+    subprocess.run(['py', 'detect.py', '--source', os.path.join(uploads_dir, secure_filename(video.filename))], shell=True)
+
+    # return os.path.join(uploads_dir, secure_filename(video.filename))
+    obj = secure_filename(video.filename)
+    return obj
+    
+@app.route("/detect2", methods=['POST'])
+def detect2():
     print("checking if valid")
     if 'video' not in request.files:
         return 'No file provided', 400
@@ -46,8 +58,8 @@ def detect():
 
     # return os.path.join(uploads_dir, secure_filename(video.filename))
     #obj = secure_filename(video.filename)
-    return url#obj
-
+    return url  #obj
+    
 @app.route("/opencam", methods=['GET'])
 def opencam():
     print("here")
@@ -58,15 +70,15 @@ def opencam():
 @app.route('/return-files', methods=['GET'])
 def return_file():
     obj = request.args.get('obj')
-    loc = os.path.join("runs/detect", obj)
+    loc = os.path.join("runs/video", obj)
     print(loc)
     try:
-        return send_file(os.path.join("runs/detect", obj), attachment_filename=obj)
+        return send_file(os.path.join("runs/video", obj), attachment_filename=obj)
         # return send_from_directory(loc, obj)
     except Exception as e:
         return str(e)
 
-# @app.route('/display/<filename>')
-# def display_video(filename):
-# 	#print('display_video filename: ' + filename)
-# 	return redirect(url_for('static/video_1.mp4', code=200))
+@app.route('/display/<filename>')
+def display_video(filename):
+ 	print('display_video filename: ' + filename)
+ 	return redirect(url_for('static/{}'.format(filename), code=200))
