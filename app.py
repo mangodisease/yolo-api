db9881b52014357bbe1359b796bacb34d4e662ef
Je Ar Becerro <jearbecerro@gmail.com>
2023-06-09 08:18:48 +0800

    init

---
+++ app.py
@@ -0,0 +1,72 @@
+from re import DEBUG, sub
+from flask import Flask, render_template, request, redirect, send_file, url_for
+from werkzeug.utils import secure_filename, send_from_directory
+import os
+import subprocess
+import boto3
+
+app = Flask(__name__)
+
+session = boto3.session.Session()
+e_url ="https://csucc.sgp1.digitaloceanspaces.com"
+client = session.client('s3',
+                        region_name='sgp1',
+                        endpoint_url=e_url,
+                        aws_access_key_id="DO00VPT27JE4BC4JV9Z6",
+                        aws_secret_access_key="Ym1/UTBzW+05lKIuL6LuYaVZ1H8D1h/Of7W8nOZF1jA")
+
+uploads_dir = os.path.join(app.instance_path, 'uploads')
+
+os.makedirs(uploads_dir, exist_ok=True)
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route("/detect", methods=['POST'])
+def detect():
+    print("checking if valid")
+    if 'video' not in request.files:
+        return 'No file provided', 400
+    file = request.files['video']
+    if file.filename == '':
+        return 'Invalid file', 400
+    print("uploading")
+    client.put_object(Body=file.read(), ACL='public-read', Bucket='yolo', Key='detect.mp4')
+    url = e_url + "/yolo/detect.mp4"
+    print(url)
+    print("detecting objects")
+    #video = request.files['video']
+    #video.save(os.path.join(uploads_dir, secure_filename(video.filename)))
+    #print(video)
+    #subprocess.run("ls", shell=True)
+    #os.path.join(uploads_dir, secure_filename(video.filename))
+    subprocess.run(['py', 'detect.py', '--source', url], shell=True)
+
+    # return os.path.join(uploads_dir, secure_filename(video.filename))
+    #obj = secure_filename(video.filename)
+    return url#obj
+
+@app.route("/opencam", methods=['GET'])
+def opencam():
+    print("here")
+    subprocess.run(['py', 'detect.py', '--source', '0'], shell=True)
+    return "done"
+    
+
+@app.route('/return-files', methods=['GET'])
+def return_file():
+    obj = request.args.get('obj')
+    loc = os.path.join("runs/detect", obj)
+    print(loc)
+    try:
+        return send_file(os.path.join("runs/detect", obj), attachment_filename=obj)
+        # return send_from_directory(loc, obj)
+    except Exception as e:
+        return str(e)
+
+# @app.route('/display/<filename>')
+# def display_video(filename):
+# 	#print('display_video filename: ' + filename)
+# 	return redirect(url_for('static/video_1.mp4', code=200))
